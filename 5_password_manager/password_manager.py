f1b10c72e767550860201f662278dfeb391ed419
guneeshvats <engg.intern29@coschool.ai>
2026-01-01 19:52:18 +0530

    password manager game

--- 5_password_manager/password_manager.py
+++ 5_password_manager/password_manager.py
@@ -1,15 +1,36 @@
+from cryptography.fernet import Fernet
 # Here we are going to organise and store all the passwords in a txt file 
 # we are going to encrypt the password and then one master password to decrypt all the paswords
 
 # First we won't encrypt anything and then we'll do the encryption one 
 
-pwd = input("What is the master password: ")
+master_pwd = input("What is the master password: ")
 
-mode = input("Would you like to add a new password or view the existing ones (view, add): ")
+# key + password ---> is what we will use to encrypt and decrypt the passwords
 
-if mode == "view":
-    pass
-elif mode == "add":
-    pass
-else:
-    print("Invaid mode")
+def view(): 
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, pwd = data.split(" | ")
+            print(f"User: {user} | Password: {pwd}")
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+    # with keyword is used to open a file and it will automatically close the file after the block of code is executed
+    # append mode is used to add new lines to the file - most flexible mode to use - we can write from the end of the file and not overwrite the existing data
+    with open("passwords.txt", 'a') as f:
+        f.write(name + " | " + pwd + "\n")
+
+while True: 
+    mode = input("Would you like to add a new password or view the existing ones (view, add, q): ").lower()
+    if mode == "q":
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invaid mode")
+        continue
